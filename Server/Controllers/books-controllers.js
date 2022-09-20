337fea9c290ef3874362f8dd867efb4cee1b21ca
Your Name <you@example.com>
2022-09-20 09:23:05 -0500

    created books-controllers content

--- Server/Controllers/books-controllers.js
+++ Server/Controllers/books-controllers.js
@@ -1,33 +1,3 @@
-1.) Create folder structure. Create folders titled "Client" for frontend, and "Server" for backend.
-
-2.) Create folder structure for "Server" folder:
-    - Inside of "Server" folder, create:
-        - "Controller"
-        - "Model"
-        - Routes
-
-3.) Create json.
-    - cd Server
-    - npm init -y
-
-4.) Install backend dependencies:
-    - npm install mongoose cors express nodemon
-
-5.) Modify backend package.json for nodemon index.js
-
-6.) Create node server:
-    - Create index.js
-    
-7.) Configure database component:
-    - Set up Atlas, create cluster, create database. titled as Bookstore, create collection titled Information.
-
-8.) Creage mongoose model for schema
-    - Create a file titled information.js in model folder
-
-9.) Create controllers file titled "books-controllers.js" in controllers folder.
-Content for books-controllers.js:
-
-
 const Information = require("../Model/Information")
 
 const getAllBooks = async(req, res, next) => {
@@ -106,7 +76,3 @@ exports.getAllBooks = getAllBooks
 exports.addBook = addBook
 exports.deleteBook = deleteBook
 exports.updateBook = updateBook
-
-
-
-
